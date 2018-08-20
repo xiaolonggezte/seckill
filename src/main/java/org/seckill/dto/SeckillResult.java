fc59f79bf4bfcb52e3f8190b4132d90bca2c869a
肖龙 <xiaolong05@meituan.com>
2018-08-21 00:03:33 +0800

    添加web层-springMVC

---
+++ src/main/java/org/seckill/dto/SeckillResult.java
@@ -0,0 +1,46 @@
+package org.seckill.dto;
+
+/**
+ * @author: xiaolong
+ * @Date: 上午12:43 2018/8/19
+ * @Description: 所有ajax操作返回 封装json结果
+ */
+public class SeckillResult<T> {
+    private boolean success;
+    private T data;
+    private String error;
+
+    public SeckillResult(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    public SeckillResult(boolean success,String error) {
+        this.success = success;
+        this.error = error;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
---
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -0,0 +1,116 @@
+package org.seckill.web;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExcution;
+import org.seckill.dto.SeckillResult;
+import org.seckill.entity.Seckill;
+import org.seckill.enums.SeckillStateEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.rmi.server.ExportException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author: xiaolong
+ * @Date: 上午12:04 2018/8/19
+ * @Description:
+ */
+
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+//        list.jsp + Model = ModelAndView
+        //获取列表页
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+
+        if (null == seckillId) {
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if (null == seckill) {
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+    //ajax json
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"}
+    )
+    @ResponseBody//封装返回json的注解
+    public SeckillResult<Exposer> exposer(@PathVariable("seckillId") Long seckillId) {
+
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true, exposer);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result = new SeckillResult<Exposer>(false, e.getMessage());
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExcution> execute(@PathVariable("seckillId") Long seckillId,
+                                                  @PathVariable("md5") String md5,
+                                                  @CookieValue(value = "killPhone", required = false) Long userPhone) {
+        if (userPhone == null) {
+            return new SeckillResult<SeckillExcution>(false, "未注册/登陆");
+        }
+        try {
+            SeckillExcution seckillExcution = seckillService.executeSeckill(seckillId, userPhone, md5);
+            return new SeckillResult<SeckillExcution>(true, seckillExcution);
+        } catch (SeckillCloseException e) {
+            SeckillExcution seckillExcution = new SeckillExcution(seckillId, SeckillStateEnum.END);
+            return new SeckillResult<SeckillExcution>(false, seckillExcution);
+        } catch (RepeatKillException e) {
+            SeckillExcution seckillExcution = new SeckillExcution(seckillId, SeckillStateEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExcution>(false, seckillExcution);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            SeckillExcution seckillExcution = new SeckillExcution(seckillId, SeckillStateEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExcution>(false, seckillExcution);
+        }
+
+    }
+
+    @RequestMapping(value = "/time/now", method = RequestMethod.GET)
+    @ResponseBody
+    public SeckillResult<Long> time() {
+        Date now = new Date();
+        return new SeckillResult<Long>(true, now.getTime());
+    }
+}
